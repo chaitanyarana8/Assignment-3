526e39c43be0ca766fbfbd7f73b11402716e0389
chaitanyarana8 <chaitanyarana.mnd@gmail.com>
2025-12-17 14:34:49 +0530

    Updated Task_1 and Task_2 solutions

--- Task_1.py
+++ Task_1.py
@@ -1,20 +1,12 @@
-def my_factorial(n):
-    """Calculate the factorial of a non-negative integer n."""
-    if n < 0:
-        return "Factorial is not defined for negative numbers."
-    fact = 1
+def factorial(n):
+    if n <0:
+        return "Factorial not defined for negative numbers"
+    result =1
     for i in range(1,n+1):
-        fact *= i
-    return fact
+        result *=i
+    return result
     
-while True:
-    try:
-        user_input = int(input("Enter a non-negative integer to calculate its factorial:"))
-        if user_input<0:
-            print("Please enter a non-negative integer.")
-        else:
-            break
-    except ValueError:
-        print("Invalid input. Please enter a valid non-negative integer.")
-
-print(f"factorial of {user_input} is: {my_factorial(user_input)}")
+# function call 
+num = int(input())
+print(f"Enter a number: {num}")
+print(f"Factorial of {num} is:" , factorial(num))
